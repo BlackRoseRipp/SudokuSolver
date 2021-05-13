98e62ccc7836016ef7ecaea45e1106a6668ed401
mperez44 <60362837+mperez44@users.noreply.github.com>
2021-05-13 15:40:54 -0400

    Update sudokuSolver.py

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -4,22 +4,22 @@ import puzzle
 print("Welcome to Sudoku Solver!")
 user = input("Do you have your own puzzle?(Y/N)")
 
-puzzleBoard = []
+sudGrid = []
 if user == "Y":
     userBoard = input("Give your puzzle as an array of rows that have an array of columns each: ")
-    puzzleBoard = userBoard
+    sudGrid = userBoard
 else:
-    puzzleBoard = puzzle.getPuzzle()
+    sudGrid = puzzle.getPuzzle()
 
-puzzle.printPuzzle(puzzleBoard)
+puzzle.printPuzzle(sudGrid)
 
 giveUp = input("Do you wish to give up? (Y/N)")
 while(giveUp != "Y"):
     giveUp = input("Do you wish to give up yet? (Y/N)")
 
 print("-------------------------------------------------")
-if (solver.SudukoSolver(puzzleBoard, 0, 0)):
+if (solver.SudukoSolver(sudGrid, 0, 0)):
     print("Here is your completed puzzle!")
-    puzzle.printPuzzle(puzzleBoard)
+    puzzle.printPuzzle(sudGrid)
 else:
     print("Solution does not exist :(")
